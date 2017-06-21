a7c6e17be0563128209d5c3a2cd8b6a0b86cac63
drhbigdave <davidrhagan@gmail.com>
2017-06-20 21:33:14 -0400

    fixed float to number conversion

--- measurement_put_item.py
+++ measurement_put_item.py
@@ -18,29 +18,43 @@ dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
 
 table = dynamodb.Table('wellness')
 
-realm = input('Will you be capturing - measurements - supplements - pain - or 5:2fast?  ')
-currentTime = datetime.today()
-timestamp = str(currentTime)
-fat = input('what fat % did the tanita scale indicate (taken to 1 decimal place)? ')
-hydration = input('what hydration value did the tanita scale indicate (taken to 1 decimal place)? ')
+#is the timestamp current or historical
+currentOrNot = input('are you inputting historical or\
+ current data? provide "c" for current, "h" for a noncurrent entry:  ')
+if currentOrNot is 'c':
+    timestamp = str(datetime.today())
+else:
+    timestamp = str(input('provide the time for the data\
+     input - \'year-month-day hour:min:sec\' example: \'2017-05-27 13:06:01:    '))
+
+realm = input('Will you be capturing - measurements - \
+    supplements - pain - or 5:2fast?  ')
+fat = input('what fat % did the tanita scale indicate\
+ (taken to 1 decimal place)? ')
+hydration = input('what hydration value did the tanita scale \
+    indicate (taken to 1 decimal place)? ')
 notes = input('are there any notes you\'d like to capture? ')
-waist = input('in inches, what was your waist measurement (taken to 1 decimal place)? ')
-weight = input('in lbs, what weight did the tanita scale indicate (taken to 1 decimal place)?')
-rtgrip = input('in lbs, what was your right grip strength measurment (taken to 1 decimal place)? ')
-ltgrip = input('in lbs, what was your left grip strength measurment (taken to 1 decimal place)? ')
+waist = input('in inches, what was your waist measurement \
+    (taken to 1 decimal place)? ')
+weight = input('in lbs, what weight did the tanita scale \
+    indicate (taken to 1 decimal place)? ')
+rtgrip = input('in lbs, what was your right grip strength \
+    measurment (taken to 1 decimal place)? ')
+ltgrip = input('in lbs, what was your left grip strength \
+    measurment (taken to 1 decimal place)? ')
 
 
 response = table.put_item(
    Item={
         'realm': realm,
         'timestamp': timestamp,
-        'fat': fat,
-        'hydration': hydration,
+        'fat': decimal.Decimal(fat),
+        'hydration': decimal.Decimal(hydration),
         'notes': notes,
-        'waist': waist,
-        'weight': weight,
-        'right-grip': rtgrip,
-        'left-grip': ltgrip
+        'waist': decimal.Decimal(waist),
+        'weight': decimal.Decimal(weight),
+        'right-grip': decimal.Decimal(rtgrip),
+        'left-grip': decimal.Decimal(ltgrip)
         }
 )
 
--- supplement_put_item.py
+++ supplement_put_item.py
@@ -18,10 +18,16 @@ dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
 
 table = dynamodb.Table('wellness')
 
+#is the timestamp current or historical
+currentOrNot = input('are you inputting historical or current data?\
+ provide "c" for current, "h" for a noncurrent entry:  ')
+if currentOrNot is 'c':
+    timestamp = str(datetime.today())
+else:
+    timestamp = str(input('provide the time for the data input -\
+     \'year-month-day hour:min:sec\' example: \'2017-05-27 13:06:01:    '))
+
 realm = 'supplements'
-#currentTime = datetime.today()
-#timestamp = str(currentTime)
-timestamp = '2017-05-26 17:34:55.647274'
 acetylCarnitine = 500
 aspirin = 81
 betaAlanine = 2250
@@ -34,6 +40,10 @@ rezV = 400
 tyrosine = 500
 ubiquinol = 100
 vitex = 450
+tribulus = 2250
+vitamind = 400
+creatine = 5000
+rhodiolaRosea = 500
 
 response = table.put_item(
    Item={
@@ -50,7 +60,11 @@ response = table.put_item(
         'tyrosine': tyrosine,
         'ubiquinol': ubiquinol,
         'acetyl-carnitine': acetylCarnitine,
-        'vitex': vitex
+        'vitex': vitex,
+        'tribulus': tribulus,
+        'creatine': creatine,
+        'vitaminD': vitamind,
+        'rhodiola-rosea': rhodiolaRosea
         }
 )
 
